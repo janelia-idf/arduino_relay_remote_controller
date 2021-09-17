f2d8fbd0e6b94e9b6b127cfee597d1cfff9d58f9
Peter Polidoro <peter@polidoro.io>
2021-09-17 09:56:40 -0400

    Update email address

--- host/python/setup.py
+++ host/python/setup.py
@@ -24,7 +24,7 @@ setup(
     name='arduino_relay_remote_controller',
     version='0.1.0',
     author='Peter Polidoro',
-    author_email='peterpolidoro@gmail.com',
+    author_email='peter@polidoro.io',
     packages=['arduino_relay_remote_controller',
               'arduino_relay_remote_controller/webserver',
               ],
